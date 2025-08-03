bcd0a00daa0ddc6f7de7779b82c98a0f4c220075
Aleksey Platonov <Alex.Platonov91@yandex.ru>
2025-08-03 17:53:03 +0400

    feat: fix

--- inner/employee/repository.go
+++ inner/employee/repository.go
@@ -22,49 +22,49 @@ type EmployeeEntity struct {
 }
 
 // AddEmployee - добавить новый элемент в коллекцию
-func (r *EmployeeRepository) AddEmployee(name string) (employee *EmployeeEntity, err error) {
-	err = r.db.Get(&employee, "INSERT INTO employee(name) VALUES(?) RETURNING *", name)
-	return employee, err
+func (er *EmployeeRepository) AddEmployee(employee *EmployeeEntity) (e EmployeeEntity, err error) {
+	err = er.db.Get(&e, "INSERT INTO employee(name) VALUES(?) RETURNING *", employee.Name)
+	return e, err
 }
 
 // FindById - найти элемент коллекции по его id (этот метод мы реализовали на уроке)
-func (r *EmployeeRepository) FindById(id int64) (employee *EmployeeEntity, err error) {
-	err = r.db.Get(&employee, "SELECT * FROM employee e WHERE e.is_deleted = FALSE AND e.id=?", id)
-	return employee, err
+func (er *EmployeeRepository) FindById(id int64) (e EmployeeEntity, err error) {
+	err = er.db.Get(&e, "SELECT * FROM employee e WHERE e.is_deleted = FALSE AND e.id=?", id)
+	return e, err
 }
 
 // FindAll - найти все элементы коллекции
-func (r *EmployeeRepository) FindAll() (employees []*EmployeeEntity, err error) {
-	err = r.db.Select(&employees, "SELECT * FROM employee e WHERE e.is_deleted = FALSE")
+func (er *EmployeeRepository) FindAll() (employees []EmployeeEntity, err error) {
+	err = er.db.Select(&employees, "SELECT * FROM employee e WHERE e.is_deleted = FALSE")
 	return employees, err
 }
 
 // FindByIds - найти слайс элементов коллекции по слайсу их id
-func (r *EmployeeRepository) FindByIds(ids []int64) (employees []*EmployeeEntity, err error) {
+func (er *EmployeeRepository) FindByIds(ids []int64) (employees []EmployeeEntity, err error) {
 	query, args, errQueryBuild := sqlx.In(
 		"SELECT * FROM employee e WHERE e.is_deleted = FALSE AND e.id IN (?)",
 		ids)
 	if errQueryBuild != nil {
 		return nil, errQueryBuild
 	}
-	err = r.db.Select(&employees, query, args...)
+	err = er.db.Select(&employees, query, args...)
 	return employees, err
 }
 
 // DeleteByIdSilent - удалить элемент коллекции по его id
-func (r *EmployeeRepository) DeleteByIdSilent(id int64) (err error) {
-	_, err = r.db.Exec("UPDATE employee e SET is_deleted = TRUE WHERE e.is_deleted = FALSE and e.id = ?", id)
+func (er *EmployeeRepository) DeleteByIdSilent(id int64) (err error) {
+	_, err = er.db.Exec("UPDATE employee e SET is_deleted = TRUE WHERE e.is_deleted = FALSE and e.id = ?", id)
 	return err
 }
 
 // DeleteByIdsSilent - удалить элементы по слайсу их id
-func (r *EmployeeRepository) DeleteByIdsSilent(ids []int64) (err error) {
+func (er *EmployeeRepository) DeleteByIdsSilent(ids []int64) (err error) {
 	query, args, errQueryBuild := sqlx.In(
 		"UPDATE employee e SET is_deleted = TRUE WHERE e.is_deleted = FALSE and e.id IN (?)",
 		ids)
 	if errQueryBuild != nil {
 		return errQueryBuild
 	}
-	_, err = r.db.Exec(query, args...)
+	_, err = er.db.Exec(query, args...)
 	return err
 }
--- inner/role/repository.go
+++ inner/role/repository.go
@@ -22,45 +22,45 @@ type RoleEntity struct {
 }
 
 // AddRole - добавить новый элемент в коллекцию
-func (r *RoleRepository) AddRole(name string) (role *RoleEntity, err error) {
-	err = r.db.Get(&role, "INSERT INTO role(name) VALUES(?) RETURNING *", name)
-	return role, err
+func (rr *RoleRepository) AddRole(role *RoleEntity) (r RoleEntity, err error) {
+	err = rr.db.Get(&r, "INSERT INTO role(name) VALUES(?) RETURNING *", role.Name)
+	return r, err
 }
 
 // FindById - найти элемент коллекции по его id (этот метод мы реализовали на уроке)
-func (r *RoleRepository) FindById(id int64) (role *RoleEntity, err error) {
-	err = r.db.Get(&role, "SELECT * FROM role rl WHERE rl.is_deleted = FALSE AND rl.id=?", id)
+func (rr *RoleRepository) FindById(id int64) (role RoleEntity, err error) {
+	err = rr.db.Get(&role, "SELECT * FROM role rl WHERE rl.is_deleted = FALSE AND rl.id=?", id)
 	return role, err
 }
 
 // FindAll - найти все элементы коллекции
-func (r *RoleRepository) FindAll() (roles []*RoleEntity, err error) {
-	err = r.db.Select(&roles, "SELECT * FROM role rl WHERE rl.is_deleted = FALSE")
+func (rr *RoleRepository) FindAll() (roles []RoleEntity, err error) {
+	err = rr.db.Select(&roles, "SELECT * FROM role rl WHERE rl.is_deleted = FALSE")
 	return roles, err
 }
 
 // FindByIds - найти слайс элементов коллекции по слайсу их id
-func (r *RoleRepository) FindByIds(ids []int64) (roles []*RoleEntity, err error) {
+func (rr *RoleRepository) FindByIds(ids []int64) (roles []RoleEntity, err error) {
 	query, args, errQueryBuild := sqlx.In("SELECT * FROM role rl WHERE rl.is_deleted = FALSE AND rl.id IN (?)", ids)
 	if errQueryBuild != nil {
 		return nil, errQueryBuild
 	}
-	err = r.db.Select(&roles, query, args...)
+	err = rr.db.Select(&roles, query, args...)
 	return roles, err
 }
 
 // DeleteByIdSilent - удалить элемент коллекции по его id
-func (r *RoleRepository) DeleteByIdSilent(id int64) (err error) {
-	_, err = r.db.Exec("UPDATE role rl SET is_deleted = TRUE WHERE rl.is_deleted = FALSE and rl.id = ?", id)
+func (rr *RoleRepository) DeleteByIdSilent(id int64) (err error) {
+	_, err = rr.db.Exec("UPDATE role rl SET is_deleted = TRUE WHERE rl.is_deleted = FALSE and rl.id = ?", id)
 	return err
 }
 
 // DeleteByIdsSilent - удалить элементы по слайсу их id
-func (r *RoleRepository) DeleteByIdsSilent(ids []int64) (err error) {
+func (rr *RoleRepository) DeleteByIdsSilent(ids []int64) (err error) {
 	query, args, errQueryBuild := sqlx.In("UPDATE role rl SET is_deleted = TRUE WHERE rl.is_deleted = FALSE and rl.id IN (?)", ids)
 	if errQueryBuild != nil {
 		return errQueryBuild
 	}
-	_, err = r.db.Exec(query, args...)
+	_, err = rr.db.Exec(query, args...)
 	return err
 }
